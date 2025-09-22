6b2b32b32a38dc8a50e1c1b820487c57ca79989d
Nancy Mohammad Dayekh <nancydayekh43@gmail.com>
2025-09-22 15:09:30 +0300

    edit send message

--- src/app/api/send-sms.js
+++ src/app/api/send-sms.js
@@ -2,7 +2,6 @@ import twilio from "twilio";
 
 export async function POST(req) {
   const { to, body } = await req.json();
-
   const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
   try {
@@ -11,12 +10,9 @@ export async function POST(req) {
       from: process.env.TWILIO_PHONE_NUMBER,
       to
     });
-
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (err) {
     console.error("Twilio Error:", err);
-    return new Response(JSON.stringify({ error: "Failed to send SMS" }), {
-      status: 500,
-    });
+    return new Response(JSON.stringify({ error: "Failed to send SMS" }), { status: 500 });
   }
 }
