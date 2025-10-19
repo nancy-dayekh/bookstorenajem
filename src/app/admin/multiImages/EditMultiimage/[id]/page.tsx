6473b3ec727a10a867c869f305eb068a5f9e1648
Nancy Mohammad Dayekh <nancydayekh43@gmail.com>
2025-10-19 16:39:33 +0300

    edit multimage

--- src/app/admin/multiImages/EditMultiimage/[id]/page.tsx
+++ src/app/admin/multiImages/EditMultiimage/[id]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useState, useEffect, ChangeEvent } from "react";
 import { supabase } from "../../../../../../lib/supabaseClient";
